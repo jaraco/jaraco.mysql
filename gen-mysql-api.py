7aaca29feb525eaf4090b8ad58073e9b4cf47c03
jaraco <none@none>
2009-07-13 23:28:49 +0000

    Further refactoring

--- gen-mysql-api.py
+++ gen-mysql-api.py
@@ -22,78 +22,72 @@ import os
 import subprocess
 from ctypeslib import h2xml, xml2py
 
-print('Generating xml')
+setup_root = os.path.dirname(__file__)
+sys.path.append(os.path.join(setup_root, 'root'))
+from _mysql_api_util import get_mysql_root
 
 platform_map = dict(
 	linux2 = 'unix',
 	darwin = 'unix',
 )
 
-setup_root = os.path.dirname(__file__)
-
 def merge_args(common_args, variable_args):
 	for args in variable_args:
 		yield common_args + args
 
-def h2xml_cmd(args):
-	assert h2xml.compile_to_xml(args) is None, cmd + ' failed'
-
-def xml2py_cmd(args):
-	assert xml2py.main(args) is None, cmd + ' failed'
-
-def gen_libs_win32():
-	sys.path.append(os.path.join(setup_root, 'root'))
-	from _mysql_api_util import get_mysql_root
-	mysql_include = os.path.join(get_mysql_root(), 'include')
-	os.environ['PATH'] += ';%s\lib\opt' % get_mysql_root()
-	common_args = ['h2xml.py', '-I', mysql_include]
-	cmds = [
+class LibGenerator(object):
+	h2xml_cmds = [
 		# need WIN32_LEAN_AND_MEAN to exclude most windows stuff
-		'-D WIN32_LEAN_AND_MEAN config-win.h mysql.h -o mysql.xml'.split(),
-		'-c errmsg.h -o errmsg.xml'.split(),
-		'-c mysql_version.h -o mysql_version.xml'.split(),
-		'-c mysqld_error.h -o mysqld_error.xml'.split(),
-	]
-	cmds = merge_args(common_args, cmds)
-	map(h2xml_cmd, cmds)
-
-	common_args = ['xml2py.py']
-	
-	cmds = [
-		'mysql.xml -l libmysql.dll -o _mysql_api_win32.py'.split(),
-		# Use -s MYSQL to get the MYSQL structure and ancestral structures
-		'errmsg.xml -o root/_mysql_errmsg.py'.split(),
-		'mysql_version.xml -o root/_mysql_version.py'.split(),
-		'mysqld_error.xml -o root/_mysql_errors.py'.split(),
-		]
-	cmds = merge_args(common_args, cmds)
-	map(xml2py_cmd, cmds)
-	patch_mysql_api()
-
-def gen_libs_unix():
-	mysql_include = '/usr/include/mysql'
-	common_args = ['h2xml.py', '-I', mysql_include]
-	cmds = [
 		'mysql.h -o mysql.xml'.split(),
 		'-c errmsg.h -o errmsg.xml'.split(),
 		'-c mysql_version.h -o mysql_version.xml'.split(),
 		'-c mysqld_error.h -o mysqld_error.xml'.split(),
 	]
-	cmds = merge_args(common_args, cmds)
-	map(h2xml_cmd, cmds)
-
-	common_args = ['xml2py.py']
-
-	cmds = [
-		'mysql.xml -l libmysqlclient.so -o _mysql_api_unix.py'.split(),
+	xml2py_cmds = [
+		'mysql.xml -l %(libname)s -o root/_mysql_api.py'.split(),
+		# Use -s MYSQL to get the MYSQL structure and ancestral structures
 		'errmsg.xml -o root/_mysql_errmsg.py'.split(),
 		'mysql_version.xml -o root/_mysql_version.py'.split(),
 		'mysqld_error.xml -o root/_mysql_errors.py'.split(),
 		]
-	cmds = merge_args(common_args, cmds)
-	map(xml2py_cmd, cmds)
 	
-	patch_mysql_api()
+	def h2xml_cmd(self, args):
+		assert h2xml.compile_to_xml(args) is None,  cmd + ' failed'
+
+	def xml2py_cmd(self, args):
+		assert xml2py.main(args) is None, cmd + ' failed'
+
+	def run(self):
+		print('Generating xml')
+		h2xml_common = ['h2xml.py', '-I', self.mysql_include]
+		cmds = merge_args(h2xml_common, self.h2xml_cmds)
+		map(self.h2xml_cmd, cmds)
+		
+		print('Generating Python libs')
+		xml2py_common = ['xml2py.py']
+		xml2py_cmds = self.xml2py_cmds
+		xml2py_cmds[0] = xml2py_cmds[0] % self.__dict__
+		cmds = merge_args(xml2py_common, self.xml2py_cmds)
+		map(self.xml2py_cmd, cmds)
+		
+		patch_mysql_api()
+
+class Win32LibGenerator(LibGenerator):
+	platform = 'win32'
+	libname='libmysql.dll'
+	mysql_include = os.path.join(get_mysql_root(), 'include')
+	# need WIN32_LEAN_AND_MEAN to exclude most windows stuff
+	h2xml_cmds = LibGenerator.h2xml_cmds
+	h2xml_cmds[0][:0] = '-D WIN32_LEAN_AND_MEAN config-win.h'.split()
+
+	def run(self):
+		os.environ['PATH'] += ';%s\lib\opt' % get_mysql_root()
+		super(Win32LibGenerator, self).run()
+
+class UnixLibGenerator(LibGenerator):
+	platform = 'unix'
+	libname='libmysqlclient.so'
+	mysql_include = '/usr/include/mysql'
 
 def patch_mysql_api():
 	"""
@@ -107,4 +101,4 @@ def patch_mysql_api():
 def get_platform_name():
 	return platform_map.get(sys.platform, sys.platform)
 
-vars()['gen_libs_'+get_platform_name()]()
+vars()[get_platform_name().capitalize()+'LibGenerator']().run()
